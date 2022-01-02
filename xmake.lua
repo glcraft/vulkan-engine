15089ac99f13220fc2478c8c3a449da3c9a99faf
Gly <gabin.lefranc@gmail.com>
2022-01-02 23:17:05 +0100

    Add C++20 flag

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,13 @@ add_requires("vulkan-headers", "vulkan-hpp", "vulkan-validation-layers", "glfw")
 
 target("vulkan-engine")
     set_kind("binary")
+    set_languages("cxx20")
     add_files("src/*.cpp")
     add_includedirs("include")
-    add_packages("vulkan-headers", "vulkan-hpp", "vulkan-validation-layers", "glfw")
+    add_defines("VULKAN_HPP_NO_CONSTRUCTORS")
+    if is_mode("debug") then
+        add_defines("DEBUG")
+        set_symbols("debug")
+        set_optimize("none")
+    end
+
